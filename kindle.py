a52bed2f7fc208545c81bb3d965717599666681a
pedroalc <alcantarapedro69@yahoo.com>
2022-05-30 20:24:27 -0300

    OK lista de parecidos

--- kindle.py
+++ kindle.py
@@ -4,7 +4,8 @@ import sys
 from easygui import *
 import subprocess
 import os
- 
+from difflib import get_close_matches
+
 message_kindle_not_found = "Kindle não encontrado! \nInforme o caminho do arquivo 'My Clippings': "
 
 title = "KINPY"
@@ -12,16 +13,16 @@ title = "KINPY"
 whoami='whoami'
 
 data = subprocess.Popen([whoami], stdout = subprocess.PIPE)
-  
+
 output = str(data.communicate())
 
 
 try:
-	arq_kindle=open('/media/pedro/Kindle/documents/My Clippings.txt', 'r')	
+	arq_kindle=open('/media/pedro/Kindle/documents/My Clippings.txt', 'r')
 except IOError:
 	janela_1 = msgbox(message_kindle_not_found, title)
 	Message_caminho = "Digite o caminho do arquivo: "
-    
+
 	output = enterbox(Message_caminho, title)
 	print(output)
 	try:
@@ -31,12 +32,19 @@ except IOError:
 	quit()
 
 arq_final=open('criados.txt', 'w')
-string="Dostoevsky: The Seeds of Revolt"
+#livro_nome=str(input("Digite o nome do livro: "))
+string="Dostoevsk: The Seeds of Revolt"
+ar2=arq_kindle.readlines()
 count=0
 ilinhas = iter(arq_kindle)
+test=False
 for linha in ilinhas:
 	if string in linha:
-		next(ilinhas)
+		test=True
+		data_raw=next(ilinhas)
+		ind=data_raw.find('Added on ')
+		data_pronta=data_raw[ind+9:]
+
 		next(ilinhas)
 		a=next(ilinhas)
 		if len(a)==1:
@@ -44,10 +52,16 @@ for linha in ilinhas:
 			continue
 		else:
 			count+=1
-			write='#'+str(count)+'\n'+'\n'
+			write='#'+str(count)+' '+data_pronta+'\n'+'\n'
 			arq_final.write(write)
 			arq_final.write(a)
 			arq_final.write('\n')
-	
+if test==False:
+	list_of_close_matches=get_close_matches(string, ar2)
+	print('Não foram encontrados livros com esse titulo. \nAqui está alguns com nomes parecidos')
+	for i in list_of_close_matches:
+		print('-> '+ i.strip())
+
+
 arq_kindle.close()
 arq_final.close()
