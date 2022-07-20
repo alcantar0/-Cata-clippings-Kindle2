e12300b11d4696ee91c46a2daf3e5e6d1d4d20cd
pedro <alcantarapedro69@yahoo.com>
2022-07-20 13:47:28 -0300

    Resolvido: não retornar nada

--- kindle.py
+++ kindle.py
@@ -1,67 +1,70 @@
 # -*- coding: utf-8 -*-
- #/media/pedro/Kindle/documents/
-import sys
-from easygui import *
+# /media/pedro/Kindle/documents/
 import subprocess
-import os
+import sys
 from difflib import get_close_matches
 
-message_kindle_not_found = "Kindle não encontrado! \nInforme o caminho do arquivo 'My Clippings': "
+message_kindle_not_found = (
+    "Kindle não encontrado! \nInforme o caminho do arquivo 'My Clippings': "
+)
 
 title = "KINPY"
 
-whoami='whoami'
+whoami = "whoami"
 
-data = subprocess.Popen([whoami], stdout = subprocess.PIPE)
+data = subprocess.Popen([whoami], stdout=subprocess.PIPE)
 
 output = str(data.communicate())
 
 
 try:
-	arq_kindle=open('/media/pedro/Kindle/documents/My Clippings.txt', 'r')
+    with open("/media/pedro/Kindle/documents/My Clippings.txt", "r") as arq_kindle:
+        ar2 = arq_kindle.readlines()
 except IOError:
-	janela_1 = msgbox(message_kindle_not_found, title)
-	Message_caminho = "Digite o caminho do arquivo: "
+    janela_1 = msgbox(message_kindle_not_found, title)
+    Message_caminho = "Digite o caminho do arquivo: "
 
-	output = enterbox(Message_caminho, title)
-	print(output)
-	try:
-		arq_kindle=open(output.encode('utf-8'), 'r')
-	except IOError:
-		output = msgbox("Caminho não encontrado.", title)
-	quit()
+    output = enterbox(Message_caminho, title)
+    print(output)
+    try:
+        with open(output, "r") as arq_kindle:
+            ar2 = arq_kindle.readlines()
+    except IOError:
+        output = msgbox("Caminho não encontrado.", title)
+    sys.exit()
 
-arq_final=open('criados.txt', 'w')
-#livro_nome=str(input("Digite o nome do livro: "))
-string="Dostoevsk: The Seeds of Revolt"
-ar2=arq_kindle.readlines()
-count=0
-ilinhas = iter(arq_kindle)
-test=False
+livro_nome = str(input("Digite o nome do livro: "))
+count = 0
+ilinhas = iter(ar2)
+test = False
 for linha in ilinhas:
-	if string in linha:
-		test=True
-		data_raw=next(ilinhas)
-		ind=data_raw.find('Added on ')
-		data_pronta=data_raw[ind+9:]
+    if livro_nome in linha:
+        test = True
+        data_raw = next(ilinhas)
+        ind = data_raw.find("Added on ")
+        data_pronta = data_raw[ind + 9 :]
 
-		next(ilinhas)
-		a=next(ilinhas)
-		if len(a)==1:
-			arq_final.write('\n')
-			continue
-		else:
-			count+=1
-			write='#'+str(count)+' '+data_pronta+'\n'+'\n'
-			arq_final.write(write)
-			arq_final.write(a)
-			arq_final.write('\n')
-if test==False:
-	list_of_close_matches=get_close_matches(string, ar2)
-	print('Não foram encontrados livros com esse titulo. \nAqui está alguns com nomes parecidos')
-	for i in list_of_close_matches:
-		print('-> '+ i.strip())
-
+        next(ilinhas)
+        a = next(ilinhas)
+        if len(a) == 1:
+            with open(f"Highlights: {livro_nome}.txt", "w") as arq_final:
+                arq_final.write("\n")
+                continue
+        else:
+            count += 1
+            write = "#" + str(count) + " " + data_pronta + "\n" + "\n"
+            with open(f"Highlights:{livro_nome}.txt", "w") as arq_final:
+                arq_final.write("\n")
+                arq_final.write(write)
+                arq_final.write(a)
+                arq_final.write("\n")
+if not test:
+    list_of_close_matches = get_close_matches(livro_nome, ar2)
+    print(
+        "Não foram encontrados livros com esse titulo. \nAqui está alguns com nomes parecidos"
+    )
+    for i in list_of_close_matches:
+        print("-> " + i.strip())
 
 arq_kindle.close()
 arq_final.close()
