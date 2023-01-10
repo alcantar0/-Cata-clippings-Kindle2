b10701ae8068b3d20ed04f2a16e85b9078fefd97
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2023-01-10 18:36:03 -0300

    Algoritmo de salvar anotações pronto

--- kindle.py
+++ kindle.py
@@ -17,50 +17,43 @@ output = str(data.communicate())
 
 
 try:
-    with open("/media/pedro/Kindle/documents/My Clippings.txt", "r") as arq_kindle:
+    with open("/home/pedro/Desktop/My Clippings.txt", "r") as arq_kindle:
         ar2 = arq_kindle.readlines()
 except IOError:
-    janela_1 = msgbox(message_kindle_not_found, title)
+    janela_1 = easygui.msgbox(message_kindle_not_found, title)
     Message_caminho = "Digite o caminho do arquivo: "
 
-    output = enterbox(Message_caminho, title)
+    output = easygui.enterbox(Message_caminho, title)
     print(output)
     try:
         with open(output, "r") as arq_kindle:
             ar2 = arq_kindle.readlines()
     except IOError:
-        output = msgbox("Caminho não encontrado.", title)
+        output = easygui.msgbox("Caminho não encontrado.", title)
     sys.exit()
 
-LIVRO_NOME= str(input("Digite o nome do livro: "))
+#LIVRO_NOME= str(input("Digite o nome do livro: "))
 CONTADOR = 0
-ilinhas = iter(ar2)
 EXISTE_ANOTACOES = False
-for linha in ilinhas:
-    if LIVRO_NOME in linha:
-        EXISTE_ANOTACOES = True
-        data_raw = next(ilinhas)
-        ind = data_raw.find("Added on ")
-        data_pronta = data_raw[ind + 9 :]
+note_indicator = False
+counter = 0
+see = True
+mark = []
 
-        next(ilinhas)
-        a = next(ilinhas)
-        if len(a) == 1:
-            with open(f"Highlights:{LIVRO_NOME}.txt", "a+") as arq_final:
-                arq_final.write("\n")
-                continue
-        else:
-            CONTADOR += 1
-            write = "#" + str(CONTADOR) + " " + data_pronta + "\n" + "\n"
-            with open(f"Highlights:{LIVRO_NOME}.txt", "a+") as arq_final:
-                arq_final.write("\n")
-                arq_final.write(write)
-                arq_final.write(a)
-                arq_final.write("\n")
-if not EXISTE_ANOTACOES:
-    list_of_close_matches = get_close_matches(LIVRO_NOME, ar2)
-    print(
-        "Não foram encontrados livros com esse titulo. \nAqui está alguns com nomes parecidos"
-    )
-    for i in list_of_close_matches:
-        print("-> " + i.strip())
+while see == True:
+    try:
+        data = ar2[counter+1]
+    except IndexError:
+        see = False
+        break
+    if data[0:8] == "- Your N":
+        dicionario = {"titulo": ar2[counter], "data": ar2[counter+1], "highlight": ar2[counter+8], "nota": ar2[counter+3]}
+        print(dicionario)
+        #print(ar2[counter+10])
+        counter +=10
+        mark.append(dicionario)
+    else:
+        dicionario = {"titulo": ar2[counter], "data": ar2[counter+1], "highlight": ar2[counter+3], "nota": "0"}
+        print(dicionario)
+        counter +=5
+        mark.append(dicionario)
